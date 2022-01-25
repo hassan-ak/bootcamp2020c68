ac036003df1f4954406acfaffad3b51c9e4a8ade
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-25 14:56:31 +0500

    Update step03_alarms-stack.ts

--- step21_cloudwatch/step03_alarms/lib/step03_alarms-stack.ts
+++ step21_cloudwatch/step03_alarms/lib/step03_alarms-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
+import * as sns from '@aws-cdk/aws-sns';
+import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
+import * as cloudwatchAction from '@aws-cdk/aws-cloudwatch-actions';
 
 export class Step03AlarmsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,54 +17,44 @@ export class Step03AlarmsStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambda'),
       handler: 'lambda.handler',
     });
+
+    // Matrices for the lambda function
+    const errors = lambdaFn.metricErrors();
+    const invocations = lambdaFn.metricInvocations();
+    const throttle = lambdaFn.metricThrottles();
+
+    // Cloudwatch variable/Matrics
+    const allProblems = new cloudwatch.MathExpression({
+      expression: 'errors + throttles',
+      usingMetrics: {
+        errors: errors,
+        throttles: throttle,
+      },
+    });
+    const problemPercentage = new cloudwatch.MathExpression({
+      expression: '(problems / invocations) * 100',
+      usingMetrics: {
+        problems: allProblems,
+        invocations: invocations,
+      },
+      period: cdk.Duration.minutes(1),
+    });
+
+    // Cloudwatch alarm
+    const alarm = new cloudwatch.Alarm(this, 'Alarm', {
+      metric: problemPercentage,
+      threshold: 10,
+      comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
+      evaluationPeriods: 1,
+    });
+
+    // SNS Topic
+    const Topic = new sns.Topic(this, 'Topic');
+    Topic.addSubscription(
+      new subscriptions.EmailSubscription('abc@example.com')
+    );
+
+    // Add alarm action to the alam
+    alarm.addAlarmAction(new cloudwatchAction.SnsAction(Topic));
   }
 }
-
-// import cloudwatch = require('@aws-cdk/aws-cloudwatch');
-// import lambda = require('@aws-cdk/aws-lambda');
-// import * as cw_actions from '@aws-cdk/aws-cloudwatch-actions';
-// import sns = require('@aws-cdk/aws-sns');
-// import { SnsAction } from '@aws-cdk/aws-cloudwatch-actions';
-// import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
-
-// export class Step03AlarmsStack extends cdk.Stack {
-//   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-//     super(scope, id, props);
-
-//     const errors = lambdaFn.metricErrors();
-//     const invocations = lambdaFn.metricInvocations();
-//     const throttle = lambdaFn.metricThrottles();
-
-//     const allProblems = new cloudwatch.MathExpression({
-//       expression: 'errors + throttles',
-//       usingMetrics: {
-//         errors: errors,
-//         throttles: throttle,
-//       },
-//     });
-
-//     const problemPercentage = new cloudwatch.MathExpression({
-//       expression: '(problems / invocations) * 100',
-//       usingMetrics: {
-//         problems: allProblems,
-//         invocations: invocations,
-//       },
-//       period: cdk.Duration.minutes(1),
-//     });
-
-//     const Topic = new sns.Topic(this, 'Topic');
-
-//     Topic.addSubscription(
-//       new subscriptions.EmailSubscription('waris.hasan00@gmail.com')
-//     );
-
-//     const alarm = new cloudwatch.Alarm(this, 'Alarm', {
-//       metric: problemPercentage,
-//       threshold: 10,
-//       comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
-//       evaluationPeriods: 1,
-//     });
-
-//     alarm.addAlarmAction(new SnsAction(Topic));
-//   }
-// }
