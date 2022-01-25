1fc64babbfde8bd8d9f11d25a8b565e3d715dc20
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-25 15:52:14 +0500

    cdk app

---
+++ step21_cloudwatch/step01_cloudwatch_alarm_with_sns/test/step01_cloudwatch_alarm_with_sns.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step01CloudwatchAlarmWithSns from '../lib/step01_cloudwatch_alarm_with_sns-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step01_cloudwatch_alarm_with_sns-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step01CloudwatchAlarmWithSns.Step01CloudwatchAlarmWithSnsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
